f58adf374573a5894e91837a457741594e4b13fe
小鹿 義晴 <yoshiharu.ojika@tiis.global>
2025-07-21 16:43:11 +0900

    リダイレクト

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,15 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import SideMenu from '../components/SideMenu';
 import MyIcon from '../components/MyIcon';
 import '../styles/global.css';
+import { getValidId } from '../utils/getValidId';
 
-function MyApp({ Component, pageProps, router }) {
+function MyApp({ Component, pageProps }) {
   const [open, setOpen] = useState(false);
+  const router = useRouter();
 
   // サイドメニューを表示しないパス一覧
   const hideSideMenuPaths = ['/login'];
 
   const showSideMenu = !hideSideMenuPaths.includes(router.pathname);
+
+  useEffect(() => {
+    // ログインページやパブリックページは除外したい場合はここで判定
+    const publicPaths = ['/login'];
+    if (publicPaths.includes(router.pathname)) return;
+
+    const id = getValidId();
+    if (!id) {
+      router.replace('/login');
+    }
+  }, [router.pathname]);
 
   return (
     <>
